67cf22a8b9521662fdac83e6fd2cdd3b6e70973e
MennaHalim <mennahalim10@gmail.com>
2024-03-18 13:16:44 +0200

    login end point

--- HubFurniture.APIs/Controllers/AccountController.cs
+++ HubFurniture.APIs/Controllers/AccountController.cs
@@ -4,6 +4,10 @@ using HubFurniture.Core.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HubFurniture.APIs.Controllers
 {
@@ -38,6 +42,56 @@ namespace HubFurniture.APIs.Controllers
                 return BadRequest(result.Errors);
             }
             return BadRequest(ModelState);
+        }
+
+        [HttpPost("login")]//api/account/login
+        public async Task<IActionResult> Login(LoginUserDto userDto)
+        {
+            if (ModelState.IsValid == true)
+            {
+                //check - create token
+                ApplicationUser user = await usermanger.FindByEmailAsync(userDto.Email);
+                if (user != null)//user name found
+                {
+                    bool found = await usermanger.CheckPasswordAsync(user, userDto.Password);
+                    if (found)
+                    {
+                        //Claims Token
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+                        //get role
+                        var roles = await usermanger.GetRolesAsync(user);
+                        foreach (var itemRole in roles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, itemRole));
+                        }
+                        SecurityKey securityKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"]));
+
+                        SigningCredentials signincred =
+                            new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                        //Create token
+                        JwtSecurityToken mytoken = new JwtSecurityToken(
+                            issuer: config["JWT:ValidIssuer"],
+                            audience: config["JWT:ValidAudiance"],
+                            claims: claims,
+                            expires: DateTime.Now.AddHours(1),
+                            signingCredentials: signincred
+                            );
+                        return Ok(new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(mytoken),
+                            expiration = mytoken.ValidTo
+                        });
+                    }
+                }
+                return Unauthorized();
+
+            }
+            return Unauthorized();
         }
     }
 }
