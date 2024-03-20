d13fdd251b90493993f961b47b37acd4b80c3399
Fahd Mohammed <150238704+FahdEldowy@users.noreply.github.com>
2024-03-20 23:48:30 +0200

    test

--- HubFurniture.APIs/Program.cs
+++ HubFurniture.APIs/Program.cs
@@ -15,7 +15,7 @@ namespace HubFurniture.APIs
     {
         public static async Task Main(string[] args)
         {
-
+            var x = 10;
             var builder = WebApplication.CreateBuilder(args);
 
             #region Configure Services
