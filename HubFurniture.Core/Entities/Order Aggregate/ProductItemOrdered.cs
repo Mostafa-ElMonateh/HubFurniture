aeda4ab5d9a7691bdd383a2b2da9ec1adc226ca8
Mostafa_Ahmed <mostafa.ahmed.mostafa726@gmail.com>
2024-03-20 19:25:19 +0200

    Create Order module entities

---
+++ HubFurniture.Core/Entities/Order Aggregate/ProductItemOrdered.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubFurniture.Core.Entities.Order_Aggregate
+{
+    public class ProductItemOrdered
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public ICollection<string> PictureUrls { get; set; }
+
+        public ProductItemOrdered(int productId, string productName)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrls = new List<string>();
+        }
+
+        public ProductItemOrdered()
+        {
+            
+        }
+    }
+}
