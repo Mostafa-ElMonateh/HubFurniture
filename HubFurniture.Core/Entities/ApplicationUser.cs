12b243965156e286393f0c2a72d7b00b6da39bfd
MennaHalim <mennahalim10@gmail.com>
2024-03-25 16:36:11 +0200

    add adress entity

--- HubFurniture.Core/Entities/ApplicationUser.cs
+++ HubFurniture.Core/Entities/ApplicationUser.cs
@@ -10,7 +10,7 @@ namespace HubFurniture.Core.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
+        // Navigational Property 1-M => [M]
+        public ICollection<Address> Addresses { get; set; } = new HashSet<Address>();
     }
 }
---
+++ HubFurniture.Repository/Data/Config/AdressConfigrations.cs
@@ -0,0 +1,29 @@
+﻿using HubFurniture.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubFurniture.Repository.Data.Config
+{
+    internal class AdressConfigrations : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.Property(a => a.StreetAdress)
+                 .IsRequired();
+
+            builder.Property(a => a.City)
+                 .IsRequired();
+
+            builder.Property(a => a.Country)
+                 .IsRequired();
+
+            builder.HasOne(a => a.User)
+                .WithMany(u => u.Addresses);
+        }
+    }
+}
