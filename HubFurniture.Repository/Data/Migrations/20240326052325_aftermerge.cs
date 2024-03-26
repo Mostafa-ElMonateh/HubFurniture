910504714e7db9a2cfc66f7df5d2c9f4cfcb7e2d
MennaHalim <mennahalim10@gmail.com>
2024-03-26 07:29:03 +0200

    handle migration

---
+++ HubFurniture.Repository/Data/Migrations/20240326052325_aftermerge.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HubFurniture.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class aftermerge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BasketId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasketId",
+                table: "AspNetUsers");
+        }
+    }
+}
