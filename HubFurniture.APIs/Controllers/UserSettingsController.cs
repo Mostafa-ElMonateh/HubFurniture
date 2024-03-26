4323c3834b655052271b74b92f94d0acb1522174
MennaHalim <mennahalim10@gmail.com>
2024-03-26 19:38:58 +0200

    address crud APIs

--- HubFurniture.APIs/Controllers/UserSettingsController.cs
+++ HubFurniture.APIs/Controllers/UserSettingsController.cs
@@ -118,5 +118,90 @@ namespace HubFurniture.APIs.Controllers
             return BadRequest(ModelState);
         }
 
+        [HttpGet("GetAllAddresses")]
+        public async Task<IActionResult> GetAllAddresses()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var addresses = await _addressService.GetAdressesForUserAsync(currentUser.Id);
+
+            var addressDtos = _mapper.Map<List<UserAddressDto>>(addresses);
+
+            return Ok(addressDtos);
+        }
+
+        [HttpPut("UpdateAddress")]
+        public async Task<IActionResult> UpdateAddress(UserAddressDto userAddressDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentUser = await _userManager.GetUserAsync(User);
+                if (currentUser == null)
+                {
+                    return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                }
+
+                var updatedAddress = _mapper.Map<UserAddress>(userAddressDto);
+                updatedAddress.UserId = currentUser.Id;
+
+                await _addressService.UpdateAddressAsync(updatedAddress);
+
+                return Ok("Address updated successfully.");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("GetAddressById/{addressId}")]
+        public async Task<IActionResult> GetAddressById(int addressId)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var address = await _addressService.GetAdressByIdForUserAsync(addressId);
+            if (address == null)
+            {
+                return NotFound($"Address with ID '{addressId}' not found for the current user.");
+            }
+
+            var addressDto = _mapper.Map<UserAddressDto>(address);
+
+            return Ok(addressDto);
+        }
+
+        [HttpDelete("DeleteAddress/{addressId}")]
+        public async Task<IActionResult> DeleteAddress(int addressId)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var address = await _addressService.GetAdressByIdForUserAsync(addressId);
+            if (address == null)
+            {
+                return NotFound($"Address with ID '{addressId}' not found for the current user.");
+            }
+
+            if (address.UserId != currentUser.Id)
+            {
+                return Forbid(); 
+            }
+
+            await _addressService.DeleteAdressByIdForUserAsync(addressId);
+
+            return Ok("Address deleted successfully.");
+        }
+
+
+
     }
 }
