a42b4b29aa2d78a73f324522e9effb16578bb1dd
MennaHalim <mennahalim10@gmail.com>
2024-04-03 12:32:12 +0200

    commit2

--- AdminPanel/Area/Identity/Pages/Account/Login.cshtml.cs
+++ AdminPanel/Area/Identity/Pages/Account/Login.cshtml.cs
@@ -14,15 +14,16 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using HubFurniture.Core.Entities;
 
 namespace AdminPanel.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
@@ -106,6 +107,8 @@ namespace AdminPanel.Areas.Identity.Pages.Account
             returnUrl ??= Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            Console.Write(ModelState.IsValid);
 
             if (ModelState.IsValid)
             {
